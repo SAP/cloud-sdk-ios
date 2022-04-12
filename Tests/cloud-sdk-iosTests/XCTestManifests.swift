6139940700963a95970df197f5ed5bb3dd6946bf
Marco Eidinger <eidingermarco@gmail.com>
2022-04-12 11:33:51 -0700

    chore: vent binary frameworks (requires auth)

--- Tests/cloud-sdk-iosTests/XCTestManifests.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-
-#if !canImport(ObjectiveC)
-    public func allTests() -> [XCTestCaseEntry] {
-        return [
-            testCase(cloud_sdk_iosTests.allTests),
-        ]
-    }
-#endif
