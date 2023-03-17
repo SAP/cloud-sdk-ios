24011a9ba0319449370b87eebc5a4bdce21fce1b
i518713 <vinay.madiwalkar@sap.com>
2023-03-17 17:19:18 +0530

    [update] version update 8.0.8

--- Package.swift
+++ Package.swift
@@ -1,21 +1,21 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.3
 
 import PackageDescription
 
-let version = "9.0.5"
-let root = "https://rbsc.repositories.cloud.sap/nexus3/repository/maven73555000100900006345/com/sap/mobile/platform/client/hcp/sdk/ios"
+let version = "8.0.8"
+let root = "https://rbsc.repositories.cloud.sap/nexus3/repository/maven73554900100900006843/com/sap/mobile/platform/client/hcp/sdk/ios"
 
-let sapCommonChecksum = "34714ea210eb696ba72ffda26920c0ac977e558033330b1ad19f7535b313186f"
-let sapFioriChecksum = "c9af5c5d0372c08fa471dec2c74457cc10fece4aa43cfdba224b09daa0e7fd5f"
-let sapFioriFlowsChecksum = "a55f7589411128ba2547fa0e47943d207ed40d6997ffba5a7de58669d891f61c"
-let sapFoundationChecksum = "e0696c963be6baedd4214a765e220dcc07fcc65e9b323887232288778efcb459"
-let sapODataChecksum = "15171eff0ece629fed200574cdbc02519ba80d805efcf6c5f65d3e81c5c7aeff"
-let sapOfflineODataChecksum = "51faa75530700faa0b213eaff18cd241c36680af45174f3297f3755034128c72"
-let sapMLChecksum = "19ef255448d8e7db5059783d3e89bf2666276f5d25f82162369d9e2f2c6a1d03"
+let sapCommonChecksum = "c70417f5eba49f32ebbc6339ccc030d2a3f7d970c15ed4594c9dab2788f58a3b"
+let sapFioriChecksum = "afff7d03fa9f46644f41936c0a6ca0d0d1e806db6cdede5031fba18ca57910f7"
+let sapFioriFlowsChecksum = "24ee176aba16a2bc9a4e3a4e44cd6c9b4a52f1a4ff8fdec275936fabbd16aca6"
+let sapFoundationChecksum = "2dd042960bd7c39d08b0a7d0e4fa4e121ce8ec676887bdbc5a00093d880b4f67"
+let sapODataChecksum = "4e45ce1b5a2c4bff06ceb1b239844988975d59c0c2da813c53f5171541fe035c"
+let sapOfflineODataChecksum = "f7c30e740fb15e7b39c96fa8c60dad417359af3d09d590ab732d8f61f65b3785"
+let sapMLChecksum = "46e24581d08f2ad1c8709aab0933d2eb770a4389aa189a8eb173f2baa199f00c"
 
 let package = Package(
     name: "cloud-sdk-ios",
-    platforms: [.iOS(.v15)],
+    platforms: [.iOS(.v14)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
